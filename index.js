1f16f50dcdd6cb22d22d759b7602b7d2e2271e78
JBWERE-coder <lutherwere@gmail.com>
2023-06-12 14:29:01 +0300

    completed assignment

---
+++ index.js
@@ -0,0 +1,25 @@
+function shout(string) {
+    console.log(string.toUpperCase);
+    return string.toUpperCase();
+  }
+  function whisper(string) {
+    return string.toLowerCase();
+  }
+  
+
+  function logShout (string) {
+    console.log(string.toUpperCase());
+  }
+
+  function logWhisper(ktown) {
+    console.log(ktown.toLowerCase());
+  }
+
+  function sayHiToHeadphonedRoommate(string) {
+    if (string === 'hello')
+     return"I can\'t hear you!"
+        else if (string === 'HELLO')
+      return "YES INDEED!"
+       else (string === "Let's have dinner together!!")
+       return "I would love to!"
+  }
